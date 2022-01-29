210dce495a8922a6790c5623db5dae0b45a113dc
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-29 15:24:28 +0530

    finishing frontend

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar";
+import ExercisesList from "./components/exercises-list";
+import EditExercise from "./components/edit-exercises";
+import CreateExercise from "./components/create-exercise";
+import CreateUser from "./components/create-user";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
